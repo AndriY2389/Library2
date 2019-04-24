cd591550632af7535a2637f8267c66dea64075a5
AndriY <andriyxxx357@gmail.com>
2019-04-24 11:05:04 +0300

    reader controller add

--- src/main/java/com/softserve/controller/ReaderController.java
+++ src/main/java/com/softserve/controller/ReaderController.java
@@ -1,10 +1,13 @@
 package com.softserve.controller;
 
+import com.softserve.model.Reader;
 import com.softserve.service.ReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReaderController {
@@ -16,5 +19,22 @@ public class ReaderController {
     public String getReader(Model model) {
         model.addAttribute("readers", readerService.findAll());
         return "reader/reader";
+    }
+
+    @PostMapping("/create_reader")
+    public String createReader(@RequestParam(name = "FirstName") String firstName,
+                               @RequestParam(name = "LastName") String lastName){
+        Reader reader = new Reader();
+        reader.setFirstName(firstName);
+        reader.setLastName(lastName);
+        readerService.save(reader);
+        return "redirect:/reader";
+    }
+
+
+    @PostMapping("/delete_reader")
+    public String deleteReader(@RequestParam(name = "id") String id){
+        readerService.deleteById(Integer.parseInt(id));
+        return "redirect:/reader";
     }
 }
